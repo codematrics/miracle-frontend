ec0558deb7b127e5f463a2365ea393662ea4add4
happy1511 <140176910+happy1511@users.noreply.github.com>
2025-08-13 02:05:01 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from "@vitejs/plugin-react"; // or vue, svelte, etc.
+import { defineConfig } from "vite";
 
-export default defineConfig({  
+export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // relative paths for assets (important for Vercel static hosting)
+});
