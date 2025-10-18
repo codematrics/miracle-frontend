cd48b06d704aeef851a9ca956bcbc7908dce7511
happy1511 <140176910+happy1511@users.noreply.github.com>
2025-10-18 22:08:44 +0530

    fix: issue

--- src/jsx/components/Dashboard/Reception/schemas/Appointment.js
+++ src/jsx/components/Dashboard/Reception/schemas/Appointment.js
@@ -1,10 +1,13 @@
 import * as Yup from 'yup';
 
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+
 export const appointmentSchema = Yup.object().shape({
   patient: Yup.string().required('Patient is required'),
   doctor: Yup.string().required('Doctor is required'),
   appointmentDate: Yup.date()
-    .min(new Date(), 'Invalid appointment date')
+    .min(today, 'Invalid appointment date')
     .required('Appointment date is required'),
   reason: Yup.string().required('Reason is required'),
 });
